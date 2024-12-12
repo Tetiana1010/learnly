26b0448a80c7c0845f89baa2f1f15c1fbe4311b1
Tetiana Kobryn <t.kobrin10@gmail.com>
2024-12-12 19:50:07 +0200

    Project setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-3xl">Hello</p>
+      <Button variant="destructive">Click me</Button>
+    </div>
+  );
+}
