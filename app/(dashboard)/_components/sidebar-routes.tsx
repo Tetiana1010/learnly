77b4f583a6c4149bbbee0a9077ee56c24a00a79c
Tetiana Kobryn <t.kobrin10@gmail.com>
2024-12-14 19:23:29 +0200

    Basic sidebar implementation

---
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Compass, Layout } from "lucide-react";
+import { SidebarItem } from "./sidebar-item";
+
+const guestsRoutes = [
+  {
+    icon: Layout, 
+    label: "Dashboard",
+    href: '/'
+  },
+  {
+    icon: Compass, 
+    label: "Browse",
+    href: '/search'
+  }
+]
+
+export const SidebarRoutes = () => {
+  const routes = guestsRoutes;
+
+  return (
+    <div className="flex flex-col w-full">
+      {
+        routes.map((route) => (
+          <SidebarItem
+            key={route.href}
+            icon={route.icon}
+            label={route.label}
+            href={route.href}
+          />
+        ))
+      }
+    </div>
+  )
+};
+
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Sidebar } from "./_components/sidebar";
+import { Navbar } from "./_components/navbar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
+      <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full x-50">
+        <Navbar />
+      </div>
       <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <Sidebar />
       </div>
-      <div className="flex flex-col w-full">
+      <main className="md:pl-56 h-full">
         { children }
-      </div>
+      </main>
     </div>
   );
 };
