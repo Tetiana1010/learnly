35aa9f6671b91b29aec310d2f9f755c361b5dd93
Tetiana Kobryn <t.kobrin10@gmail.com>
2024-12-18 22:49:58 +0200

    Course Edit Page UI

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -4,22 +4,21 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const { userId } = await auth(); 
+    const { userId } = await auth();
     const { title } = await req.json();
 
-    if(!userId){
-      return new NextResponse("Unauthorized", { status: 401});
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
 
     const course = await db.course.create({
       data: {
         userId,
         title,
-      }
+      },
     });
 
     return NextResponse.json(course);
-
   } catch (error) {
     console.log("[Courses]", error);
     return new NextResponse("Internal Error", { status: 500 });
