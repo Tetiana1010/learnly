dd2a17443f0e8012f88ee225961d5b6998e5b6d1
Tetiana Kobryn <t.kobrin10@gmail.com>
2025-01-27 10:30:00 +0200

    Add ChapterIdPage component for chapter details retrieval and user authentication

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -0,0 +1,50 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+
+const ChapterIdPage = async({
+  params
+}: {
+  params: { courseId: string, chapterId: string}
+}) => {
+
+  const { userId } = await auth();
+
+  if(!userId){
+    return redirect('/')
+  };
+
+  const chapter = await db.chapter.findUnique({
+    where: {
+      id: params.chapterId,
+      courseId: params.courseId
+    },
+    include: {
+      muxData: true
+    }
+  });
+
+  if(!chapter){
+    return redirect("/");
+  };
+
+  const requiredFields = [
+    chapter.title,
+    chapter.description,
+    chapter.videoUrl
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionText = `(${completedFields}/${totalFields})`;
+
+  return (
+    <div>
+      Chapter Id
+    </div>
+  )
+};
+
+export default ChapterIdPage;
