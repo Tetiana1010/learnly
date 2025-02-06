d26a54ae351d864b952a49f0a4e54ed1dff9b8d9
Tetiana Kobryn <t.kobrin10@gmail.com>
2025-02-06 12:00:00 +0200

    Add delete confirmation modal to chapter actions and handle chapter deletion

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/_components/chapter-actions.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import axios from "axios";
+import { ConfirmModal } from "@/components/modals/confirm-modal";
 import { Button } from "@/components/ui/button";
 import { TrashIcon } from "lucide-react";
-
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 interface ChapterActionsProps {
   disabled: boolean;
   courseId: string;
   chapterId: string;
   isPublished: boolean;
-}
+};
 
 export const ChapterActions = ({
   disabled,
@@ -16,6 +19,25 @@ export const ChapterActions = ({
   chapterId,
   isPublished,
 }: ChapterActionsProps) => {
+  const router = useRouter();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const onDelete = async () => {
+    try {
+      setIsLoaded(true);
+      await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`);
+
+      toast.success("Chapter deleted successfully");
+      router.refresh();
+      router.push(`/teacher/courses/${courseId}`);
+
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoaded(false);
+    }
+  };
+
   return (
     <div className="flex items-center gap-x-w">
       <Button
@@ -26,11 +48,11 @@ export const ChapterActions = ({
       >
         {isPublished ? "Unpublish" : "Publish"}
       </Button>
-      <Button
-        size="sm"
-      >
-        <TrashIcon className="h-4 w-4" />
-      </Button>
+      <ConfirmModal onConfirm={onDelete}>
+        <Button size="sm" disabled={isLoaded}>
+          <TrashIcon className="h-4 w-4" />
+        </Button>
+      </ConfirmModal>
     </div>
   );
 };
