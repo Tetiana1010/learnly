b597a04cde8f7cb9853b490b82392e6eced40997
Tetiana Kobryn <t.kobrin10@gmail.com>
2025-02-11 12:00:00 +0200

    Enhance course deletion by adding error handling for Mux asset deletion

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -1,60 +1,12 @@
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import Mux from "@mux/mux-node";
+import Mux, { APIError } from "@mux/mux-node";
 
 const { video } = new Mux({
   tokenId: process.env.MUX_TOKEN_ID,
   tokenSecret: process.env.MUX_TOKEN_SECRET,
 });
-
-// export async function DELETE(
-//   req: Request,
-//   { params }: { params: { courseId: string } },
-// ) {
-//   try {
-//     const { userId } = await auth();
-
-//     if (!userId) {
-//       return new NextResponse("Unauthorized", { status: 401 });
-//     };
-
-//     const course = await db.course.findUnique({
-//       where: {
-//         id: params.courseId,
-//         userId,
-//       },
-//       include: {
-//         chapters: {
-//           include: {
-//             muxData: true,
-//           },
-//         },
-//       },
-//     });
-
-//     if(!course){
-//       return new NextResponse("Not found", { status: 404 });
-//     };
-
-//     for(const chapter of course.chapters){
-//       if(chapter.muxData?.assetId){
-//         await video.assets.delete(chapter.muxData.assetId);
-//       };
-//     };
-
-//     const deletedCourse = await db.course.delete({
-//       where: {
-//         id: params.courseId,
-//       }
-//     });
-
-//     return NextResponse.json(deletedCourse);
-//   } catch (error) {
-//     console.log("[COURSE_ID]", error);
-//     return new NextResponse("Internal Error", { status: 500 });
-//   };
-// };
 
 export async function DELETE(
   req: Request,
@@ -88,7 +40,15 @@ export async function DELETE(
 
     for (const chapter of course.chapters) {
       if (chapter.muxData?.assetId) {
-        await video.assets.delete(chapter.muxData.assetId);
+        try {
+          await video.assets.delete(chapter.muxData.assetId);
+        } catch (muxError) {
+          if (muxError instanceof APIError && muxError.status === 404) {
+            console.error(`Mux asset with ID ${chapter.muxData.assetId} not found`);
+          } else {
+            throw muxError;
+          }
+        }
       }
     }
 
