854f02dee564de1f25985991b2c72737282e4dba
Tetiana Kobryn <t.kobrin10@gmail.com>
2025-02-23 12:00:00 +0200

    Add CourseCard component and update CoursesList to display courses with cards

--- components/courses-list.tsx
+++ components/courses-list.tsx
@@ -1,4 +1,5 @@
 import { Category, Course } from "@prisma/client";
+import { CourseCard } from "./course-card";
 
 type CourseWithProgressWithCategory = Course & {
   category: Category | null;
@@ -12,10 +13,26 @@ interface CoursesListProps {
 
 export const CoursesList = ({ items }: CoursesListProps) => {
   return (
-    <div>
-      {items.map((item) => (
-        <div key={item.id}>{item.title}</div>
-      ))}
-    </div>
+    <>
+      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 2xl:grid-cols-5 gap-4 w-full">
+        {items.map((item) => (
+          <CourseCard
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            imageUrl={item.imageUrl!}
+            chapterLength={item.chapters.length}
+            price={item.price!}
+            progress={item.progress}
+            category={item?.category?.name || ""}
+          />
+        ))}
+      </div>
+      {items.length === 0 && (
+        <div className="text-center text-sm text-muted-foreground mt-10">
+          No courses found
+        </div>
+      )}
+    </>
   );
 };
